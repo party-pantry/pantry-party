08c98136f250b22b368ce0c37593f4c9e425a44a
Dat Truong <datg@hawaii.edu>
2025-09-15 20:23:17 -1000

    delete

--- pantry-party/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6Hu5ikV3u8NDYhdqcv3v/jLZZFNZPctx1GC4FSBXYHc=\"\n}"
--- pantry-party/.next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8d98b8f1._.css",
-  "static/chunks/node_modules_30061e6f._.js",
-  "static/chunks/src_components_62932ca9._.js"
-],
-    source: "dynamic"
-});
