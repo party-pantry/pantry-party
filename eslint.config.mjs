d6bb459cf2b5a669bd0ab3c1579186c9976c7669
Jaren Pinera <jpinera@hawaii.edu>
2025-09-17 11:02:14 -1000

    testing ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,10 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "node_modules/",
+      "dist/ ",
+      "coverage/",
+      "generated/**"
     ],
   },
 ];
