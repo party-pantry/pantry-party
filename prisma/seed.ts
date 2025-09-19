4ac37c7ae9af57bbf7a8832ee088e955b27daa51
Dhaniel Bolosan <dhaniel808123@gmail.com>
2025-09-18 22:38:43 -1000

    Fix database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -103,7 +103,7 @@ async function main() {
         create: {
           id: user.id,
           email: user.email,
-          name: user.name,
+          username: user.username,
           password,
         }
       });
