25b298412159399baf4875296c75fba57260012f
Abigail Lorber <alorber@hawaii.edu>
2025-09-21 00:22:54 -1000

    updated sign up modal

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { Modal, Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
 import * as Yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { createUser } from '../lib/dbFunctions';
 import { signIn } from 'next-auth/react';
+// Component imports
+import { Modal, Form, Button, InputGroup } from 'react-bootstrap';
+// Icon imports
+import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
 
 interface Props {
     show: boolean;
@@ -16,18 +20,21 @@ type SignUpData = {
     username: string;
     email: string;
     password: string;
+    confirmPassword: string;
 }
 
 const SignUpModal: React.FC<Props> = ({ show, onHide }) => {
     const validationSchema = Yup.object().shape({
         username: Yup.string().required('Username is required'),
-        email: Yup.string().required('Email is required').email('Invalid email format'),
-        password: Yup.string().min(6).required('Password is required'),
+        email: Yup.string().required('Email is required').email('Email is invalid'),
+        password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters'),
+        confirmPassword: Yup.string().required('Confirm Password is required').oneOf([Yup.ref('password'), ''], 'Password does not match'),
     });
 
     const {
         register,
         handleSubmit,
+        formState: { errors },
     } = useForm<SignUpData>({
         resolver: yupResolver(validationSchema),
     });
@@ -43,27 +50,101 @@ const SignUpModal: React.FC<Props> = ({ show, onHide }) => {
         });
     };
 
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [passwordConfirmVisible, setPasswordConfirmVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    const togglePasswordConfirmVisibility = () => {
+        setPasswordConfirmVisible(!passwordConfirmVisible)
+    }
+
     return (
-        <>
-            <Modal show={show} onHide={onHide} backdrop="static" keyboard={false} centered contentClassName="custom-modal">
+            <Modal show={show} onHide={onHide} centered>
                 <Modal.Header style={{ borderBottom: "none", paddingBottom: "0px" }} closeButton />
                 <Modal.Body className="text-center">
-                    <h4>Welcome To The Party!</h4>
+                    <h5 className="text-center mb-4"><strong>Join the Party!</strong></h5>
                     <Form onSubmit={handleSubmit(onSubmit)} className="sign-up-form">
-                        <Form.Group className="mb-3" controlId="username">
-                            <Form.Control className="text-center" type="text" placeholder="Username" {...register("username")} />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="email">
-                            <Form.Control className="text-center" type="email" placeholder="Email" {...register("email")} />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="password">
-                            <Form.Control className="text-center" type="password" placeholder="Password" {...register("password")} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">Sign Up</Button>
+
+                        {/* Username */}
+                        <InputGroup className="mt-4 mb-3 custom-input-group pl-10 pr-10" >
+                            <InputGroup.Text className="custom-input-group-text">
+                                <FaUser />
+                            </InputGroup.Text>
+                            <Form.Control
+                                placeholder="Username"
+                                // className="custom-input-control"
+                                className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                                type="text"
+                                {...register('username')}
+                            />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
+                        </InputGroup>
+
+                        {/* Email */}
+                        <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
+                            <InputGroup.Text className="custom-input-group-text">
+                                <FaEnvelope />
+                            </InputGroup.Text>
+                            <Form.Control
+                                placeholder="Email"
+                                // className="custom-input-control"
+                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                                type="email"
+                                {...register('email')}
+                            />
+                            <div className="invalid-feedback">{errors.email?.message}</div>
+                        </InputGroup>
+
+                        {/* Password */}
+                        <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
+                            <InputGroup.Text className="custom-input-group-text">
+                                <FaLock />
+                            </InputGroup.Text>
+                            <Form.Control
+                                placeholder="Password"
+                                // className="custom-input-control"
+                                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                                type={passwordVisible ? 'text' : 'password'}
+                                {...register('password')}
+                            />
+                            <InputGroup.Text 
+                                className="custom-input-group-text toggle-icon" 
+                                onClick={togglePasswordVisibility}
+                            >
+                                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                            </InputGroup.Text>
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </InputGroup>
+
+                        {/* Confirm Password */}
+                        <InputGroup className="mb-7 custom-input-group pl-10 pr-10">
+                            <InputGroup.Text className="custom-input-group-text">
+                                <FaLock />
+                            </InputGroup.Text>
+                            <Form.Control
+                                placeholder="Confirm Password"
+                                // className="custom-input-control"
+                                className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
+                                type={passwordConfirmVisible ? 'text' : 'password'}
+                                {...register('confirmPassword')}
+                            />
+                            <InputGroup.Text 
+                                className="custom-input-group-text toggle-icon" 
+                                onClick={togglePasswordConfirmVisibility}
+                            >
+                                {passwordConfirmVisible ? <FaEyeSlash /> : <FaEye />}
+                            </InputGroup.Text>
+                            <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
+                        </InputGroup>
+
+                        {/* Sign Up Button */}
+                        <Button variant="dark" type="submit"><strong>Create My Account</strong></Button>
                     </Form>
                 </Modal.Body>
             </Modal>
-        </>
     );
 };
 
