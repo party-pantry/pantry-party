c4220c60c48dcd35348f9ea32df1a263347168cf
lucascorriero <lucascorriero@gmail.com>
2025-09-23 22:13:14 -1000

    landing page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,36 @@
+"use client";
 import { Container, Button } from "react-bootstrap";
 import FeaturesSection from "../components/FeaturesSection";
+import Image from 'next/image';
+import { useState } from "react";
+import SignInModal from "../components/SignInModal";
+import SignUpModal from "../components/SignUpModal";
 
 export default function Home() {
+  const [showSignIn, setShowSignIn] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+
   return (
-      <Container>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", textAlign: "center", height: "100vh" }}>
-          <h1>The Fun Way To Stock Your Pantry</h1>
-          <h5>Easily track and manage your pantry items across multiple kitches and storage spaces.</h5>
-          <Button>Get Started</Button>
+    <>
+      <SignInModal show={showSignIn} onHide={() => setShowSignIn(false)} />
+      <SignUpModal show={showSignUp} onHide={() => setShowSignUp(false)} />
+      <Container fluid>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", height: "100vh", padding: "0 5%",}}>
+          <div style={{ flex: 1, textAlign: "left" }}>
+            <h1 style={{ marginBottom: '24px' }}>The Fun Way To Stock Your Pantry</h1>
+            <h5 style={{ marginBottom: '24px' }}>Easily track and manage your pantry items across multiple kitchens and storage spaces</h5>
+            <Button variant="outline-primary" size="sm" onClick={() => setShowSignIn(true)} style={{ marginRight: '12px' }}>Sign In</Button>
+            <Button variant="primary" size="sm" onClick={() => setShowSignUp(true)}>Sign Up</Button>
+          </div>
+          <div style={{ flex: 1, textAlign: "center" }}>
+            <Image src="/landing.jpg" alt="Image of food items" width={1100} height={600} priority />
+          </div>
         </div>
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center" }} className="mb-5">
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center",}} className="mb-5">
           <h1>Features</h1>
           <FeaturesSection />
         </div>
       </Container>
+    </>
   );
 }
