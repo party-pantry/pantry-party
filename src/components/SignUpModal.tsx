1a92c3332d9eb94fbdd33d00c6d1817481c485d5
Abigail Lorber <alorber@hawaii.edu>
2025-09-23 22:03:19 -1000

    updated add item form

--- src/components/SignUpModal.tsx
+++ src/components/SignUpModal.tsx
@@ -62,89 +62,89 @@ const SignUpModal: React.FC<Props> = ({ show, onHide }) => {
     }
 
     return (
-            <Modal show={show} onHide={onHide} centered>
-                <Modal.Header style={{ borderBottom: "none", paddingBottom: "0px" }} closeButton />
-                <Modal.Body className="text-center">
-                    <h5 className="text-center mb-4"><strong>Join the Party!</strong></h5>
-                    <Form onSubmit={handleSubmit(onSubmit)} className="sign-up-form">
+        <Modal show={show} onHide={onHide} centered>
+            <Modal.Header style={{ borderBottom: "none", paddingBottom: "0px" }} closeButton />
+            <Modal.Body className="text-center">
+                <h5 className="text-center mb-4"><strong>Join the Party!</strong></h5>
+                <Form onSubmit={handleSubmit(onSubmit)} className="sign-up-form">
 
-                        {/* Username */}
-                        <InputGroup className="mt-4 mb-3 custom-input-group pl-10 pr-10" >
-                            <InputGroup.Text className="custom-input-group-text">
-                                <FaUser />
-                            </InputGroup.Text>
-                            <Form.Control
-                                placeholder="Username"
-                                // className="custom-input-control"
-                                className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-                                type="text"
-                                {...register('username')}
-                            />
-                            <div className="invalid-feedback">{errors.username?.message}</div>
-                        </InputGroup>
+                    {/* Username */}
+                    <InputGroup className="mt-4 mb-3 custom-input-group pl-10 pr-10" >
+                        <InputGroup.Text className="custom-input-group-text">
+                            <FaUser />
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Username"
+                            // className="custom-input-control"
+                            className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                            type="text"
+                            {...register('username')}
+                        />
+                        <div className="invalid-feedback">{errors.username?.message}</div>
+                    </InputGroup>
 
-                        {/* Email */}
-                        <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
-                            <InputGroup.Text className="custom-input-group-text">
-                                <FaEnvelope />
-                            </InputGroup.Text>
-                            <Form.Control
-                                placeholder="Email"
-                                // className="custom-input-control"
-                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                                type="email"
-                                {...register('email')}
-                            />
-                            <div className="invalid-feedback">{errors.email?.message}</div>
-                        </InputGroup>
+                    {/* Email */}
+                    <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
+                        <InputGroup.Text className="custom-input-group-text">
+                            <FaEnvelope />
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Email"
+                            // className="custom-input-control"
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                            type="email"
+                            {...register('email')}
+                        />
+                        <div className="invalid-feedback">{errors.email?.message}</div>
+                    </InputGroup>
 
-                        {/* Password */}
-                        <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
-                            <InputGroup.Text className="custom-input-group-text">
-                                <FaLock />
-                            </InputGroup.Text>
-                            <Form.Control
-                                placeholder="Password"
-                                // className="custom-input-control"
-                                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                                type={passwordVisible ? 'text' : 'password'}
-                                {...register('password')}
-                            />
-                            <InputGroup.Text 
-                                className="custom-input-group-text toggle-icon" 
-                                onClick={togglePasswordVisibility}
-                            >
-                                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
-                            </InputGroup.Text>
-                            <div className="invalid-feedback">{errors.password?.message}</div>
-                        </InputGroup>
+                    {/* Password */}
+                    <InputGroup className="mb-3 custom-input-group pl-10 pr-10">
+                        <InputGroup.Text className="custom-input-group-text">
+                            <FaLock />
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Password"
+                            // className="custom-input-control"
+                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                            type={passwordVisible ? 'text' : 'password'}
+                            {...register('password')}
+                        />
+                        <InputGroup.Text 
+                            className="custom-input-group-text toggle-icon" 
+                            onClick={togglePasswordVisibility}
+                        >
+                            {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                        </InputGroup.Text>
+                        <div className="invalid-feedback">{errors.password?.message}</div>
+                    </InputGroup>
 
-                        {/* Confirm Password */}
-                        <InputGroup className="mb-7 custom-input-group pl-10 pr-10">
-                            <InputGroup.Text className="custom-input-group-text">
-                                <FaLock />
-                            </InputGroup.Text>
-                            <Form.Control
-                                placeholder="Confirm Password"
-                                // className="custom-input-control"
-                                className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
-                                type={passwordConfirmVisible ? 'text' : 'password'}
-                                {...register('confirmPassword')}
-                            />
-                            <InputGroup.Text 
-                                className="custom-input-group-text toggle-icon" 
-                                onClick={togglePasswordConfirmVisibility}
-                            >
-                                {passwordConfirmVisible ? <FaEyeSlash /> : <FaEye />}
-                            </InputGroup.Text>
-                            <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
-                        </InputGroup>
+                    {/* Confirm Password */}
+                    <InputGroup className="mb-7 custom-input-group pl-10 pr-10">
+                        <InputGroup.Text className="custom-input-group-text">
+                            <FaLock />
+                        </InputGroup.Text>
+                        <Form.Control
+                            placeholder="Confirm Password"
+                            // className="custom-input-control"
+                            className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
+                            type={passwordConfirmVisible ? 'text' : 'password'}
+                            {...register('confirmPassword')}
+                        />
+                        <InputGroup.Text 
+                            className="custom-input-group-text toggle-icon" 
+                            onClick={togglePasswordConfirmVisibility}
+                        >
+                            {passwordConfirmVisible ? <FaEyeSlash /> : <FaEye />}
+                        </InputGroup.Text>
+                        <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
+                    </InputGroup>
 
-                        {/* Sign Up Button */}
-                        <Button variant="dark" type="submit"><strong>Create My Account</strong></Button>
-                    </Form>
-                </Modal.Body>
-            </Modal>
+                    {/* Sign Up Button */}
+                    <Button variant="dark" type="submit"><strong>Create My Account</strong></Button>
+                </Form>
+            </Modal.Body>
+        </Modal>
     );
 };
 
