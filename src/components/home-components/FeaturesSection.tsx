3c21c33e79ec9f463d5daa6ac3af11373d60e460
Dhaniel Bolosan <dhaniel808123@gmail.com>
2025-09-30 14:49:41 -1000

    Improvement to homepage features

--- src/components/home-components/FeaturesSection.tsx
+++ src/components/home-components/FeaturesSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Container, Row, Col } from 'react-bootstrap';
-import { House, Package2, RefreshCw, ShoppingCart, DollarSign, Search } from 'lucide-react';
+import { House, ShoppingCart, CookingPot, RefreshCw, Search, DollarSign } from 'lucide-react';
 import FeatureCard from './FeatureCard';
 
 const features = [
@@ -11,9 +11,14 @@ const features = [
     icon: <House />,
   },
   {
-    title: 'Custom Storage Spaces',
-    description: 'Add custom storage spaces and categorize your items for easy access.',
-    icon: <Package2 />,
+    title: 'Shopping List Generation',
+    description: 'Automatically see what’s running low and plan your shopping.',
+    icon: <ShoppingCart />,
+  },
+  {
+    title: 'Discover New Recipes',
+    description: 'Explore user-created meals and find recipes you can make with ingredients you already have.',
+    icon: <CookingPot />,
   },
   {
     title: 'Real-Time Inventory Updates',
@@ -21,20 +26,15 @@ const features = [
     icon: <RefreshCw />,
   },
   {
-    title: 'Shopping List Generation',
-    description: 'Automatically see what’s running low and plan your shopping.',
-    icon: <ShoppingCart />,
+    title: 'Quick Search Filters',
+    description: 'Find any item quickly with search and filter options.',
+    icon: <Search />,
   },
   {
     title: 'Instant Cost Estimates',
     description: 'Know how much you’ll spend if you restock everything.',
     icon: <DollarSign />,
-  },
-  {
-    title: 'Quick Search Filters',
-    description: 'Find any item quickly with search and filter options.',
-    icon: <Search />,
-  },
+  }
 ];
 
 const FeaturesSection: React.FC = () => (
