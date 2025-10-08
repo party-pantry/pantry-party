41980f2e43e3672591622a6082681331d40880fa
Jaren Pinera <jpinera@hawaii.edu>
2025-10-08 02:46:02 -1000

    Testing search

--- src/components/recipes-components/RecipesSearch.tsx
+++ src/components/recipes-components/RecipesSearch.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { Search } from 'lucide-react';
 
-const RecipesSearch: React.FC = () => (
-         <div className="position-relative" style={{ maxWidth: '250px' }}>
+interface RecipesSearchProps {
+  onApply?: (search: string) => void;
+}
+
+const RecipesSearch: React.FC<RecipesSearchProps> = ({ onApply }) => {
+  const [search, setSearch] = useState('');
+
+  //   const handleReset = () => {
+  //     setSearch('');
+  //     if (onApply) onApply( {search: ''} );
+  //   };
+
+  const handleApply = () => {
+    if (onApply) onApply(search);
+  };
+
+  return (
+        <div className="position-relative" style={{ maxWidth: '250px' }}>
             <Search size={18} className="position-absolute top-50 translate-middle-y ms-2 text-muted" />
                 <Form.Control
                     placeholder="Search..."
-                    style={{ paddingLeft: '2rem' }}
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleApply();
+              }
+            }}
+            style={{ paddingLeft: '2rem' }}
                 />
         </div>
-);
+  );
+};
 
 export default RecipesSearch;
