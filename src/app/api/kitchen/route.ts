cf6d2152913271d5b15ec415a20a993d0ff90148
Dat Truong <datg@hawaii.edu>
2025-10-07 16:52:22 -1000

    Force new user to add house implemented

--- src/app/api/kitchen/route.ts
+++ src/app/api/kitchen/route.ts
@@ -2,10 +2,17 @@ import { NextResponse } from 'next/server';
 import { prisma } from '../../../lib/prisma';
 
 // eslint-disable-next-line import/prefer-default-export
-export async function GET() {
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const id  = searchParams.get('userId');
+
+  if (!id) {
+    return NextResponse.json({ error: 'Missing userId' }, { status: 400 });
+  }
+
   try {
     const houses = await prisma.house.findMany({
-      where: { userId: 1 }, // implement session userId when auth is added
+      where: { userId: Number(id) },
       include: {
         storages: {
           include: { stocks: { include: { ingredient: true } } },
