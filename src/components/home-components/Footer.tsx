51db3527a87c5a0ea4c740d2530a7c2ee644fbbe
Jaren Pinera <jpinera@hawaii.edu>
2025-10-13 23:30:39 -1000

    Added custom 404 Error Message page
    When user tries to access a page w/o signing in

--- src/components/home-components/Footer.tsx
+++ src/components/home-components/Footer.tsx
@@ -5,18 +5,20 @@ import { Github } from 'lucide-react';
 import Link from 'next/link';
 
 const Footer: React.FC = () => (
-  <footer id="footer" className="custom-footer bg-primary py-5">
+  <footer id="footer" className="custom-footer bg-primary py-3">
     <Container>
-      <Row>
-        <Col>
-          <h5>Quick Links</h5>
-          <ul className="list-unstyled">
-            <li><Link href="/#home" className="text-white text-decoration-none">Home</Link></li>
-            <li><Link href="/#features" className="text-white text-decoration-none">Features</Link></li>
-          </ul>
+      <Row className="">
+        <Col className="text-end ">
+          <h5>Quick Links<strong> |</strong></h5>
         </Col>
+          <Col className="text-start">
+            <ul className="list-unstyled">
+              <li><Link href="/#home" className="text-white text-decoration-none">Home</Link></li>
+              <li><Link href="/#features" className="text-white text-decoration-none">Features</Link></li>
+            </ul>
+          </Col>
       </Row>
-      <Row className="mt-4">
+      <Row className="mt-2">
         <Col>
           <p style={{ display: 'inline-flex', alignItems: 'center', gap: '0.3rem' }}>
             Managed by Pantry Party Organization
